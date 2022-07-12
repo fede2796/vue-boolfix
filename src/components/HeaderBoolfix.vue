54d77c9002a0aa40f212990bb19d5759bbdd1e46
fede2796 <fede27396@gmail.com>
2022-07-12 14:56:42 +0200

    start exercise

---
+++ src/components/HeaderBoolfix.vue
@@ -0,0 +1,27 @@
+<template>
+<header>
+    <div>
+        <h1>BoolFlix</h1>
+    </div>
+</header>
+</template>
+
+<script>
+</script>
+
+<style lang="scss" scoped>
+    header{
+        height: 80px;
+        background-color: black;
+        display: flex;
+        justify-content: center;
+        justify-content: space-between;
+
+        h1{
+            text-transform: uppercase;
+            font-size: 28px;
+            padding: 0 30px;
+            color: red;
+        }
+    }
+</style>
